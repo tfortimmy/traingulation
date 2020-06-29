6ad4c1ebe2413a194df93083ab73c591d56da44a
tfortimmy <timkrenton@gmail.com>
2020-06-30 09:02:10 +1000

    Updating tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,24 +1,24 @@
 from unittest import TestCase
-from triangulation.utils import Line, intersection, Triangle
+from triangulation.utils import Line, Triangle
 
 
-class TestIntersection(TestCase):
+class TestLine(TestCase):
 
     def test_parallel(self):
-        self.assertFalse(
-            intersection(
-                Line(0, 0, 0, 1, 1, 1),
-                Line(0, 0, 0, 1, 1, 1)
-            )
+        ix, iy = Line(0, 0, 0, 1, 1, 1).intersection(
+            Line(1, 1, 1, 2, 2, 2)
         )
 
+        self.assertIsNone(ix)
+        self.assertIsNone(iy)
+
     def test_orthogonal(self):
-        self.assertTrue(
-            intersection(
-                Line(0, -1, -1, 1, 1, 1),
-                Line(0, -1, 1, 1, 1, -1)
-            )
+        ix, iy = Line(0, -1, -1, 1, 1, 1).intersection(
+            Line(0, -1, 1, 1, 1, -1)
         )
+
+        self.assertEqual(ix, 0)
+        self.assertEqual(iy, 0)
 
     def test_is_close(self):
 
@@ -26,9 +26,9 @@ class TestIntersection(TestCase):
         p3 = [.35251871, 0.0675376]
         p4 = [0.62635059, 0.59866086]
 
-        self.assertFalse(
-            intersection(
-                Line(2, *p2, 4, *p4),
-                Line(2, *p2, 3, *p3)
-            )
+        ix, iy = Line(2, *p2, 4, *p4).intersection(
+            Line(2, *p2, 3, *p3)
         )
+
+        self.assertIsNone(ix)
+        self.assertIsNone(iy)
