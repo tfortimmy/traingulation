afbbe6ca63fb8395ca1847fdabc18399ac7a179d
tfortimmy <timkrenton@gmail.com>
2020-06-30 09:34:47 +1000

    Adding tests for vertical and horizontal

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -32,3 +32,78 @@ class TestLine(TestCase):
 
         self.assertIsNone(ix)
         self.assertIsNone(iy)
+
+    def test_vertical_adjacent(self):
+
+        ix, iy = Line(0, 0, 0, 1, 0, 1).intersection(
+            Line(1, 0, 1, 2, 0, 2)
+        )
+
+        self.assertIsNone(ix)
+        self.assertIsNone(iy)
+
+    def test_vertical_enclosed(self):
+
+        # the second line is included in the first line
+        ix, iy = Line(0, 0, 0, 3, 0, 3).intersection(
+            Line(1, 0, 1, 2, 0, 2)
+        )
+
+        # all have value x as 0
+        self.assertEqual(ix, 0)
+        # choose the smallest possible y value
+        self.assertEqual(iy, 1)
+
+    def test_one_vertical(self):
+        ix, iy = Line(0, 0, -1, 1, 0, 1).intersection(
+            Line(2, -1, -1, 3, 1, 1)
+        )
+
+        self.assertEqual(ix, 0)
+        self.assertEqual(iy, 0)
+
+    def test_horizontal_adjacent(self):
+
+        ix, iy = Line(0, 0, 0, 1, 1, 0).intersection(
+            Line(1, 1, 0, 2, 2, 0)
+        )
+
+        self.assertIsNone(ix)
+        self.assertIsNone(iy)
+
+    def test_horizontal_enclosed(self):
+
+        # the second line is included in the first line
+        ix, iy = Line(0, 0, 0, 3, 3, 0).intersection(
+            Line(1, 1, 0, 2, 2, 0)
+        )
+
+        # all have value y as 0
+        self.assertEqual(iy, 0)
+        # choose the smallest possible x value
+        self.assertEqual(ix, 1)
+
+    def test_one_horizontal(self):
+        ix, iy = Line(0,  -1, 0, 1,  1, 0).intersection(
+            Line(2, -1, -1, 3, 1, 1)
+        )
+
+        self.assertEqual(ix, 0)
+        self.assertEqual(iy, 0)
+
+    def test_vertical_horizontal(self):
+        # one vertical line intersecting one horizontal line
+        ix, iy = Line(0, 0, -1, 1, 0, 1).intersection(
+            Line(0,  -1, 0, 1,  1, 0)
+        )
+
+        self.assertEqual(ix, 0)
+        self.assertEqual(iy, 0)
+
+    def test_horizontal_vertical(self):
+        ix, iy = Line(0,  -1, 0, 1,  1, 0).intersection(
+            Line(0, 0, -1, 1, 0, 1)
+        )
+
+        self.assertEqual(ix, 0)
+        self.assertEqual(iy, 0)
